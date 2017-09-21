c23a4144c67e2bc2b33b5b0957f4a939a73955ae
Pierre <pierre@mbs-macbook-air.home>
2017-09-21 09:52:58 +0200

    proberen 2e scherm mr error

---
+++ app/src/main/java/com/example/pierre/movieapplicaiton/FilmActivity.java
@@ -0,0 +1,71 @@
+package com.example.pierre.movieapplicaiton;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+/**
+ * Created by Rosta on 12-11-2016.
+ */
+
+// Indien zelf aangemaakt, 'extends Activity' toevoegen
+// TODO: VOEG DE NIEUWE ACTIVITY TOE AAN HET MANIFEST manifests/AndroidManifest.xml
+// implements DetailListener pas toevoegen wanneer je het tegenkomt in het stappenplan (DetailListener)
+public class FilmActivity extends Activity implements FilmListener {
+
+    // BELANGRIJK: als je onCreate override en er zou als parameter 'PersistableBundle persistentState' bijstaan
+    // WIS DIT, alsook 'persistentState' wissen uit super.onCreate
+    // Doe je dit niet, dan start je een empty activity in plaats van diegene die je wilt starten via de intent
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.film_activity);
+
+        // Pas toevoegen wanneer je het tegenkomt in het stappenplan (DetailListener)
+        // Haal de doorgegeven positie op
+        Intent intent = getIntent();
+        // -1 is de waarde die toegekend wordt indien position niet bestaat
+        int position = intent.getIntExtra("position", -1);
+
+        // Onnodige check, aangezien de index van het geklikte item wordt doorgegeven
+        if (position != -1) {
+            setFieldsAndPicture(position);
+        }
+        // Tot hier (DetailListener)
+    }
+
+    // Deze functie pas toevoegen wanneer je het tegenkomt in het stappenplan (DetailListener)
+    @Override
+    public void setFieldsAndPicture(int position) {
+        FilmLayout filmLayout = (FilmLayout) getFragmentManager().findFragmentById(R.id.fragment_container2);
+        // Roep de functie in DetailFragment aan
+        filmLayout.setAllFieldsAndImageFromPosition(position);
+        setBackgroundImage(position);
+    }
+    public void setBackgroundImage(int position) {
+        Resources res = getResources();
+        String[] movies = res.getStringArray(R.array.titles);
+        String movie = movies[position];
+        String url = movie.toLowerCase().replace(" ","_");
+//        RelativeLayout layout =(RelativeLayout)findViewById(R.id.filmLayout);
+//       layout.setBackground(Drawable.createFromPath(url));
+        Fragment fragmentView = getFragmentManager().findFragmentById(R.layout.film_layout);
+        Fragment f= fragmentView.getFragmentManager().findFragmentById(R.id.backgroundImage);
+        Context c = f.getContext();
+        int id = c.getResources().getIdentifier(url, "drawable", fragmentView.getContext().getPackageName());
+        setBackgroundImage(id);
+
+    }
+
+
+}
--- app/src/main/java/com/example/pierre/movieapplicaiton/ListFragment.java
+++ app/src/main/java/com/example/pierre/movieapplicaiton/ListFragment.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 /**
  * Created by pierre on 20/09/2017.
  */
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.list_fragment, container, false);
@@ -26,8 +27,15 @@ public class ListFragment extends Fragment {
         MyArrayAdapter cumstomAdapter = new MyArrayAdapter(getActivity(), movies, genres);
         listView.setAdapter(cumstomAdapter);
 
-
-
+        // Deze code pas toevoegen wanneer je het tegenkomt in het stappenplan (DetailListener)
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // Wanneer er een rij aangeklikt wordt, sturen we de positie door naar de Listener
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FilmListener filmListener = (FilmListener) getActivity();
+                filmListener.setFieldsAndPicture(position);
+            }
+        });
         return view;
     }
 }
