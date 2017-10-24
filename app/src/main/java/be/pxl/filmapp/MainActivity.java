5ed151306b301b2d6ac20bad91ecfea0d549ee7f
pieterbollen87 <pieter.bollen3@gmail.com>
2017-10-24 11:24:56 +0200

    listview in werkende fragment

--- app/src/main/java/be/pxl/filmapp/MainActivity.java
+++ app/src/main/java/be/pxl/filmapp/MainActivity.java
@@ -1,11 +1,16 @@
 package be.pxl.filmapp;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentContainer;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -17,6 +22,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
@@ -29,89 +35,29 @@ import be.pxl.filmapp.services.MovieService;
 import be.pxl.filmapp.utility.AppHelper;
 import be.pxl.filmapp.utility.UserSession;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private MovieAdapter adapterMovies;
-    private IMovieService movieService;
-    private ScrollView scrollView;
-    private ListView listViewMovies;
+    MovieListFragment listFragment = new MovieListFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Button b = (Button) findViewById(R.id.addFilmButton);
-        scrollView = (ScrollView) findViewById(R.id.list_fragment);
-        listViewMovies = (ListView) scrollView.findViewById(R.id.list_fragment2);
-        EditText filterEdiText = (EditText) findViewById(R.id.filter);
-        movieService = new MovieService(AppHelper.getDb(getApplicationContext()).movieDao(), getApplicationContext());
 
-        b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddMovieActivity.class);
-                startActivity(intent);
-            }
-        });
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        listViewMovies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, MovieDetailActivity.class);
-                intent.putExtra(MovieDetailActivity.MOVIE_OBJECT, adapterMovies.getFilteredData().get(position));
-                startActivity(intent);
-            }
-        });
 
-        filterEdiText.addTextChangedListener(new TextWatcher() {
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Call back the Adapter with current character to Filter
-                adapterMovies.getFilter().filter(s.toString());
-            }
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
+        fragmentTransaction.add(R.id.MainFragmentContainer, listFragment);
+        fragmentTransaction.commit();
 
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
-
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        Button b = (Button) findViewById(R.id.addFilmButton);
         retrieveLoginSession();
-        initializeDisplayContent();
     }
-
-    private void initializeDisplayContent() {
-        new AsyncTask<Void, Void, List<MovieBean>>() {
-            @Override
-            protected List<MovieBean> doInBackground(Void... params) {
-                return movieService.getAllMovies();
-            }
-
-            @Override
-            protected void onPostExecute(List<MovieBean> movies) {
-                adapterMovies = new MovieAdapter(getApplicationContext(), movies);
-                listViewMovies.setAdapter(adapterMovies);
-
-                movieService.updateMovieDatabaseFromApi(movies);
-            }
-        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
-
-        movieService.getAllMoviesObs().observe(this, new Observer<List<MovieBean>>() {
-            @Override
-            public void onChanged(@Nullable List<MovieBean> movies) {
-                if (movies != null) {
-                    adapterMovies = new MovieAdapter(getApplicationContext(), movies);
-                    listViewMovies.setAdapter(adapterMovies);
-                }
-            }
-        });
-    }
-
     private void retrieveLoginSession() {
         SharedPreferences sharedPreferences =this.getSharedPreferences("Login", MODE_PRIVATE);
 
---
+++ app/src/main/java/be/pxl/filmapp/MovieListFragment.java
@@ -0,0 +1,129 @@
+package be.pxl.filmapp;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.Observer;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import java.util.List;
+
+import be.pxl.filmapp.adapters.MovieAdapter;
+import be.pxl.filmapp.data.bean.MovieBean;
+import be.pxl.filmapp.services.IMovieService;
+import be.pxl.filmapp.services.MovieService;
+import be.pxl.filmapp.utility.AppHelper;
+
+
+public class MovieListFragment extends Fragment  {
+    private static final String ARG_OPTION_NUMBER = "option number";
+
+    private MovieAdapter adapterMovies;
+    private IMovieService movieService;
+    private ScrollView scrollView;
+    private ListView listViewMovies;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        b.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, AddMovieActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+    private void initializeDisplayContent() {
+        new AsyncTask<Void, Void, List<MovieBean>>() {
+            @Override
+            protected List<MovieBean> doInBackground(Void... params) {
+                return movieService.getAllMovies();
+            }
+
+            @Override
+            protected void onPostExecute(List<MovieBean> movies) {
+                adapterMovies = new MovieAdapter(getContext(), movies);
+                listViewMovies.setAdapter(adapterMovies);
+
+                movieService.updateMovieDatabaseFromApi(movies);
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
+
+//        movieService.getAllMoviesObs().observe(this, new Observer<List<MovieBean>>() {
+//            @Override
+//            public void onChanged(@Nullable List<MovieBean> movies) {
+//                if (movies != null) {
+//                    adapterMovies = new MovieAdapter(getContext(), movies);
+//                    listViewMovies.setAdapter(adapterMovies);
+//                }
+//            }
+//        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View  view= inflater.inflate(R.layout.fragment_movie_list, container, false);
+//        scrollView = (ScrollView) view.findViewById(R.id.list_fragment);
+        listViewMovies = (ListView) view.findViewById(R.id.list_fragment2);
+        EditText filterEdiText = (EditText) view.findViewById(R.id.filter);
+        movieService = new MovieService(AppHelper.getDb(getContext()).movieDao(), getContext());
+
+        listViewMovies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), MovieDetailActivity.class);
+                intent.putExtra(MovieDetailActivity.MOVIE_OBJECT, adapterMovies.getFilteredData().get(position));
+                startActivity(intent);
+            }
+        });
+
+        filterEdiText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Call back the Adapter with current character to Filter
+                adapterMovies.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+
+        initializeDisplayContent();
+
+      return view;
+    }
+
+//    @NonNull
+//    @Override
+//    public Lifecycle getLifecycle() {
+//        return null;
+//    }
+}
