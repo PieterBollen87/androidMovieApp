1a9ad70e311123d1ed4169150b397c79bef0b9a6
pieterbollen87 <pieter.bollen3@gmail.com>
2017-10-30 12:37:51 +0100

    adjustment

--- app/src/main/java/be/pxl/filmapp/adapters/GridImageAdapter.java
+++ app/src/main/java/be/pxl/filmapp/adapters/GridImageAdapter.java
@@ -46,7 +46,7 @@ public class GridImageAdapter extends BaseAdapter {
             if (convertView == null) {
                 // if it's not recycled, initialize some attributes
                 imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setLayoutParams(new GridView.LayoutParams(100, 75));
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setPadding(8, 8, 8, 8);
             } else {
